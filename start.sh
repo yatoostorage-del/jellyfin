05f9e4a3f39aa0d399f70eab59ac277d914d8bef
yatoostorage-del <yatoostorage@gmail.com>
2026-03-04 14:52:40 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
-python3 /app/strm_sync.py 2>&1 | tee /proc/1/fd/1 &
-/jellyfin/jellyfin --datadir /config --cachedir /cache --ffmpeg /usr/lib/jellyfin-ffmpeg/ffmpeg
+echo "=== Starting strm_sync ===" 
+python3 /app/strm_sync.py &
+echo "=== Starting Jellyfin ==="
+exec /jellyfin/jellyfin \
+  --datadir /config \
+  --cachedir /cache \
+  --ffmpeg /usr/lib/jellyfin-ffmpeg/ffmpeg
